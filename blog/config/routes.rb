cece4d251c94859d773b96e1025eddb45eecc8c2
Thiago Fonseca <dev.thiago@gmail.com>
2021-12-24 14:55:37 -0300

    Adding rails tutorial

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/articles", to: "articles#index"
+  root "articles#index"
+  resources :articles
 end
