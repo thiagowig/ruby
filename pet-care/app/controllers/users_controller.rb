d64c9406ccad4c5d201c409f6034c56ac3dd765e
Thiago Fonseca <dev.thiago@gmail.com>
2021-12-30 20:44:12 -0300

    Adding pet-care

---
+++ pet-care/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      render @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
