a9bb422c8d08bae80cca6782453c8a1174067c75
Thiago Fonseca <dev.thiago@gmail.com>
2022-01-02 13:21:26 -0300

    Adding security

--- pet-care/app/controllers/sessions_controller.rb
+++ pet-care/app/controllers/sessions_controller.rb
@@ -8,16 +8,17 @@ class SessionsController < ApplicationController
 
     respond_to do |format|
       if user && user.authenticate(params[:session][:password])
-        notice = "Logged in successfully"
+        session[:user_id] = user.id
         format.html { redirect_to user_url(user), notice: "Logged in successfully." }
-        format.json { render :show, status: :ok, location: user }
       else
-        format.html { render :new, notice: "Logged in successfully." }
+        format.html { redirect_to login_url, status: :unprocessable_entity, alert: "Invalid email or password." }
       end
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
