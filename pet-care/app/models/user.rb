0cb3154c97d70911dbba1375ae31ac53c89abb40
Thiago Fonseca <dev.thiago@gmail.com>
2022-01-03 18:30:38 -0300

    Adding unit tests

--- pet-care/app/models/user.rb
+++ pet-care/app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
+
   before_save { self.email = email.downcase}
 
-  has_many :pets
+  has_many :pets, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   has_secure_password
--- pet-care/test/models/user_test.rb
+++ pet-care/test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Java", email: "dev.thiago@gmail.com", password: "123")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "name must be present" do
+    @user.name = nil
+    assert @user.invalid?
+  end
+
+  test "email must be present" do
+    @user.email = nil
+    assert @user.invalid?
+  end
+
+  test "email must be in the correct format" do
+    @user.email = "thiago"
+    assert @user.invalid?
+  end
+
+  test "email must be unique" do
+    @user.save
+    second_user = User.new(name: "Java", email: "dev.thiago@gmail.com", password: "123")
+    assert second_user.invalid?
+  end
+
 end
