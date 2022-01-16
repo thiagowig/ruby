af0989cf351acbeae46caf2f3b1c065a79781e67
Thiago Fonseca <dev.thiago@gmail.com>
2022-01-16 16:25:37 -0300

    Setting up Mailgun

--- pet-care/config/application.rb
+++ pet-care/config/application.rb
@@ -20,5 +20,16 @@ module PetCare
     # config.eager_load_paths << Rails.root.join("extras")
 
     Dotenv::Railtie.load
+
+    ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'rails-pet-care.heroku.com',
+      :authentication => :plain,
+    }
+    ActionMailer::Base.delivery_method = :smtp
+
   end
 end
--- pet-care/config/environments/development.rb
+++ pet-care/config/environments/development.rb
@@ -67,20 +67,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.default_url_options = {
-    :host => '127.0.0.1',
-    :protocol => 'http'
-  }
-
-  config.action_mailer.smtp_settings = {
-    :address => ENV["MAILER_SMTP_ADDRESS"],
-    :port => ENV["MAILER_SMTP_PORT"],
-    :user_name => ENV["MAILER_SMTP_USER_NAME"],
-    :password => ENV["MAILER_SMTP_PASSWORD"],
-    :authentication => 'plain',
-    :enable_starttls_auto => true
-  }
 end
