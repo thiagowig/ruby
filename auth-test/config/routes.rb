bafed2a3b01e9be483f5a13a8023a8a353b21aea
Thiago Fonseca <dev.thiago@gmail.com>
2022-02-21 16:16:17 -0300

    Initial commit

--- auth-test/config/routes.rb
+++ auth-test/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
