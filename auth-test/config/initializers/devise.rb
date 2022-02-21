04ef88b336a842daa9ca1ac8188ff44adc353ed1
Thiago Fonseca <dev.thiago@gmail.com>
2022-02-21 18:32:09 -0300

    Initial commit

--- auth-test/config/initializers/devise.rb
+++ auth-test/config/initializers/devise.rb
@@ -10,8 +10,8 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
-  # random tokens. Changing this key will render invalid all existing
-  # confirmation, reset password and unlock tokens in the database.
+  #   # random tokens. Changing this key will render invalid all existing
+  #   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '0e2742bba38b3bb5d69a1895d7206f85cc341f5ffd036b51f5b0955da1a4ed836a4bd90b76ffac047c33e6d37369efe818a5833a2cccd0e5fb6ecf9f999a1ee4'
@@ -129,10 +129,10 @@ Devise.setup do |config|
   # config.pepper = '258caa4fd5f117c632b25446b7798ca9b7c432197838ebeb5dd6c5abfdaeeb84136b0406f7536556ff42527268cc37e8873eee3f353592c7c9025d4840bf9ebd'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notification = false
+  config.send_email_changed_notification = false
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = false
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -194,27 +194,27 @@ Devise.setup do |config|
   # Defines which strategy will be used to lock an account.
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
+  config.lock_strategy = :failed_attempts
 
   # Defines which key will be used when locking and unlocking an account
-  # config.unlock_keys = [:email]
+  config.unlock_keys = [:email]
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  # config.unlock_strategy = :both
+  config.unlock_strategy = :email
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 3
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
-  # config.last_attempt_warning = true
+  config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
   #
@@ -307,5 +307,5 @@ Devise.setup do |config|
 
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
-  # config.sign_in_after_change_password = true
+  config.sign_in_after_change_password = true
 end
---
+++ auth-test/config/initializers/session_store_configuration.rb
@@ -0,0 +1,2 @@
+
+Rails.application.config.session_store :cookie_store, key: '_awesome_session', expire_after: 1.minute
